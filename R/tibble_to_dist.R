0b2454f4a56ebdb7c4adac32d4a3b69c2ce9d25b
Hidehito Okada <hidefungi@gmail.com>
2021-07-26 10:26:07 +0900

    Update tibble_to_dist.R
    fix {{ }} and set style

--- R/tibble_to_dist.R
+++ R/tibble_to_dist.R
@@ -5,23 +5,26 @@
 #' @param .col_name column name.
 #' @param .matrix output
 #' @export
-tibble_to_dist <- function (.dist_tbl, .col_name = "ses", .matrix = F)
-{
+tibble_to_dist <- function(.dist_tbl, .col_name = "ses", .matrix = F) {
   # select column.
   dist_tbl <- .dist_tbl %>%
-    select(item1, item2, .col_name)
+    select(item1, item2, {{ .col_name }})
   # select unique column.
   col <- unique(c(dist_tbl$item1, dist_tbl$item2))
   # add same pair.
-  same_pair <- tibble::tibble(item1 = col,
-                              item2 = col,
-                              .col_name = 0)
+  same_pair <- tibble::tibble(
+    item1 = col,
+    item2 = col,
+    {{ .col_name }} := 0
+  )
   # convert to matrix.
   mat <- dist_tbl %>%
     dplyr::bind_rows(same_pair) %>%
-    tidyr::pivot_wider(names_from = "item2",
-                       values_from = .col_name,
-                       values_fill = 0) %>%
+    tidyr::pivot_wider(
+      names_from = "item2",
+      values_from = .col_name,
+      values_fill = 0
+    ) %>%
     dplyr::relocate(item1, all_of(col)) %>%
     dplyr::arrange(factor(item1, col)) %>%
     tibble::column_to_rownames("item1") %>%
